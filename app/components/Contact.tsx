e91e636c6c134026c54d9c4ffa76012a7a3aa476
dbugrose <darcy.ophelia.rose@gmail.com>
2026-03-11 13:47:27 -0700

    centering adjustment

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -6,13 +6,12 @@ const Contact = () => {
   return (
         <section className="section flex justify-center flex-col place-self-center items-center min-h-screen w-[clamp(300px,80vw,800px)] text-[#d1a8da] text-center">
             <AnimatedContact/>
-        <div><section className="">
+        <div className='w-[clamp(300px,80vw,800px)]  text-center'>
         <p className='text-[clamp(18px,2vw,2vw)]'> <span className='text-[clamp(18px,3vw,3vw)] font-(family-name:--font-meow-script) text-shadow-xs text-shadow-[#4f1254]'> Email:</span> darcy.ophelia.rose@gmail.com</p>
         <p className='text-[clamp(18px,2vw,2vw)]'><span className='text-[clamp(18px,3vw,3vw)] font-(family-name:--font-meow-script) text-shadow-xs text-shadow-[#4f1254]'>GitHub:</span> github.com/dbugrose</p>
         <p className='text-[clamp(18px,2vw,2vw)]'><span className='text-[clamp(18px,3vw,3vw)] font-(family-name:--font-meow-script) text-shadow-xs text-shadow-[#4f1254]'>LinkedIn:</span> Pending</p>
         <p className='text-[clamp(18px,2vw,2vw)]'><span className='text-[clamp(18px,3vw,3vw)] font-(family-name:--font-meow-script) text-shadow-xs text-shadow-[#4f1254]'>Phone Number:</span> Pending</p>
 
-      </section>
       </div>
   
       <ArrowUp/>
